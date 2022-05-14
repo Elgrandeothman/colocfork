ce769a1f4ce51ebd90a1270e6eb61a924a41355b
Elgrandeothman <98783973+Elgrandeothman@users.noreply.github.com>
2022-05-14 19:46:45 +0000

    Delete colocV1 directory

--- colocV1/src/utils/randomImage.js
+++
@@ -1,6 +0,0 @@
-import fallback1 from '../Assets/images/fallback/fallback1.jpg';
-import fallback2 from '../Assets/images/fallback/fallback2.jpg';
-
-const RandomImages = [fallback1, fallback2];
-
-export default RandomImages;
